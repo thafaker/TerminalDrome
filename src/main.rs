b53474aa6155897cdc9407df49292f2dae518097
thafaker <thafaker@gmx.net>
2025-05-08 11:19:49 +0200

    main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod config;
 use crate::api::get_artists;
 use crate::config::AppConfig;
 use anyhow::Result;
+use std::collections::HashMap;
 use std::io::{self, Write};
 
 fn main() -> Result<()> {
@@ -12,6 +13,13 @@ fn main() -> Result<()> {
     println!("Starte Anfrage an {} als {}", config.server_url, config.username);
     print!("Lade Künstler...");
     io::stdout().flush()?;
+
+    let mut params: HashMap<&str, &str> = HashMap::new();
+    params.insert("u", &config.username[..]);
+    params.insert("p", &config.password[..]);
+    params.insert("v", "1.16.1");
+    params.insert("c", "termnavi");
+    params.insert("f", "json");
 
     let artists = get_artists(&config)?;
     println!("\r{} Künstler erhalten", artists.len());
